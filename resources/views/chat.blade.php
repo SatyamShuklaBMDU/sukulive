47514122ab8b632203cac9a197afed02f61c5312
Satyam Shukla <satyamshukla.bmdu@gmail.com>
2024-09-21 15:03:07 +0530

    Changes complete

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -1,51 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chat Debugging</title>
-</head>
-
-<body>
-    <h1>Chat Debugging - Room: {{ $senderId . '_' . $receiverId }}</h1>
-
-    <!-- Display the messages in this div -->
-    <div id="messages">
-        <h2>Messages:</h2>
-        <ul id="messageList"></ul>
-    </div>
-
-    <!-- Include Socket.io Client -->
+    <title>Socket.io Test</title>
     <script src="https://cdn.socket.io/4.0.0/socket.io.min.js"></script>
-
+</head>
+<body>
     <script>
-        var sender_id = {{ $senderId }};
-        var receiver_id = {{ $receiverId }};
-        const socket = io('http://localhost:3000');
+        const socket = io('http://13.202.220.240:3000');
 
-        const room = sender_id < receiver_id ? `room_${sender_id}_${receiver_id}` : `room_${receiver_id}_${sender_id}`;
-
-        socket.emit('joinRoom', {
-            sender_id,
-            receiver_id
+        socket.on('connect', () => {
+            console.log('Connected to Socket.io server!');
         });
-        console.log(`Joined room: ${room}`);
 
-        // Listen for messages in the room
-        socket.on('chatMessage', (data) => {
-            if (!data || !data.sender_name || !data.message) {
-                console.error('Received incomplete data:', data);
-                return;
-            }
-
-            console.log('Message received:', data);
-            const messageList = document.getElementById('messageList');
-            const li = document.createElement('li');
-            li.textContent = `From: ${data.sender_name} - Message: ${data.message} - Time: ${data.time}`;
-            messageList.appendChild(li);
+        socket.on('connect_error', (err) => {
+            console.error('Connection error:', err);
         });
     </script>
 </body>
-
 </html>
--- socket-server.js
+++ socket-server.js
@@ -2,10 +2,12 @@ const { log } = require("console");
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
 const server = http.createServer(app);
+app.use(cors());
 const io = socketIo(server, {
     cors: {
         origin: "*",
