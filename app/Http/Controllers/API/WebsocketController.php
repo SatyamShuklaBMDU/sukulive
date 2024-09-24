b6f65db279e0ba8ecbe6de40211ca651cdf3176d
Satyam Shukla <satyamshukla.bmdu@gmail.com>
2024-09-24 11:22:52 +0530

    changes complete 24-09-2024

--- app/Http/Controllers/API/WebsocketController.php
+++ app/Http/Controllers/API/WebsocketController.php
@@ -9,71 +9,46 @@ use Ratchet\MessageComponentInterface;
 
 class WebsocketController implements MessageComponentInterface
 {
-    protected $clients;
-    private $subscriptions;
-    private $users;
-    private $userresources;
-
-    public function __construct()
-    {
-        $this->clients = new \SplObjectStorage;
-        $this->subscriptions = [];
-        $this->users = [];
-        $this->userresources = [];
-    }
+    protected $clients = [];
 
     public function onOpen(ConnectionInterface $conn)
     {
-        $this->clients->attach($conn);
-        $this->users[$conn->resourceId] = $conn;
+        echo "New connection! ({$conn->resourceId})\n";
     }
-    public function onMessage(ConnectionInterface $conn, $msg)
+
+    public function onMessage(ConnectionInterface $conn, $message)
     {
-        echo $msg;
-        $data = json_decode($msg);
-
-        if (isset($data->command)) {
-            switch ($data->command) {
-                case "subscribe":
-                    // Store subscription to a conversation channel
-                    $this->subscriptions[$conn->resourceId] = $data->conversation_id;
-                    break;
-
-                case "message":
-                    // Forward message to the target conversation
-                    if (isset($this->subscriptions[$conn->resourceId])) {
-                        $conversation_id = $this->subscriptions[$conn->resourceId];
-
-                        // Broadcast the message to all clients in the same conversation
-                        foreach ($this->subscriptions as $resourceId => $subscribedConversation) {
-                            if ($subscribedConversation == $conversation_id && $resourceId != $conn->resourceId) {
-                                $this->users[$resourceId]->send(json_encode([
-                                    'conversation_id' => $conversation_id,
-                                    'sender_id' => $data->sender_id,
-                                    'sender_name' => $data->sender_name,
-                                    'message' => $data->message,
-                                    'time' => $data->time
-                                ]));
-                            }
-                        }
-                    }
-                    break;
+        $data = json_decode($message, true);
+        if ($data['command'] === 'register') {
+            $userId = $data['userId'];
+            $this->clients[$userId] = $conn;
+            echo "User $userId registered.\n";
+        }
+        if ($data['command'] === 'message') {
+            $fromUserId = $data['from'];
+            $toUserId = $data['to'];
+            $msg = $data['message'];
+            echo "Message from User $fromUserId to User $toUserId: $msg\n";
+            if (isset($this->clients[$toUserId])) {
+                $toClient = $this->clients[$toUserId];
+                $toClient->send(json_encode([
+                    'command' => 'message',
+                    'from' => $fromUserId,
+                    'message' => $msg
+                ]));
+            } else {
+                echo "User $toUserId is not connected.\n";
             }
         }
     }
-
     public function onClose(ConnectionInterface $conn)
     {
-        $this->clients->detach($conn);
         echo "Connection {$conn->resourceId} has disconnected\n";
-        unset($this->users[$conn->resourceId]);
-        unset($this->subscriptions[$conn->resourceId]);
-
-        foreach ($this->userresources as &$userId) {
-            foreach ($userId as $key => $resourceId) {
-                if ($resourceId == $conn->resourceId) {
-                    unset($userId[$key]);
-                }
+        foreach ($this->clients as $userId => $client) {
+            if ($client === $conn) {
+                unset($this->clients[$userId]);
+                echo "User $userId has been disconnected.\n";
+                break;
             }
         }
     }
