e9a7d20bcc44e1ab911daa77cffc7bec27790fa1
SatyamShuklaBMDU <satyamshukla.bmdu@gmail.com>
2024-12-02 19:01:39 +0530

    changes

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -23,7 +23,7 @@ class LoginController extends Controller
         ]);
         $credentials = $request->only('email', 'password');
         if (Auth::attempt($credentials)) {
-            return redirect()->intended('dashboard')
+            return redirect()->intended('admin/dashboard')
                         ->withSuccess('Signed in');
         }
         $validator['emailPassword'] = 'Email address or password is incorrect.';
