07ac98738dc14d20da07e1539b48f4fcdef8c99a
SatyamShuklaBMDU <satyamshukla.bmdu@gmail.com>
2024-12-13 11:42:35 +0530

    changes

--- app/Http/Controllers/API/LoginController.php
+++ app/Http/Controllers/API/LoginController.php
@@ -136,7 +136,7 @@ class LoginController extends Controller
         $login = Auth::user();
         $user = Customer::findOrFail($login->id);
         $followingCount = $user->followings()->count();
-        $followersCount = $user->withCount(['followables'])->count();
+        $followersCount = $user->withCount(['followables','followings'])->get();
         $totalPostsCount = $user->media()->where('collection_name', 'posts')->count();
         $totalPosts = $user->getMedia('posts')->map(function ($media) {
             return [
