d5a2826ac283a50820d84f7863b4859db2da25c8
SatyamShuklaBMDU <satyamshukla.bmdu@gmail.com>
2024-12-20 13:12:32 +0530

    changes

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -6,6 +6,7 @@ use BeyondCode\Comments\Traits\HasComments;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Spatie\MediaLibrary\MediaCollections\Models\Media as BaseMedia;
 use Illuminate\Database\Eloquent\Model;
+use Overtrue\LaravelLike\Like;
 use Overtrue\LaravelLike\Traits\Likeable;
 
 class Media extends BaseMedia
@@ -13,4 +14,9 @@ class Media extends BaseMedia
     use Likeable, HasComments;
 
     protected $table = 'media';
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
 }
